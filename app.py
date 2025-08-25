3902d54926483ca644efd801e3ff52819d70f0a6
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2025-08-25 18:41:47 +0530

    colors changed

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from customtkinter import *
+import components.book_icon as book_icon
 
 if __name__ == "__main__":
-    pass
+    root = CTk()
+    b1 = book_icon.Book_icon(root,"resources/Books/dr br ambedkar.jpg","Dr B.R. Ambedkar","Br ambedkar",1,12,favourite=True,reserved=False)
+    b1.pack(padx=10,pady=10)
+    root.mainloop()
--- components/book_icon.py
+++ components/book_icon.py
@@ -1,13 +1,23 @@
 from customtkinter import *
 from tkinter import *
-import colors
 
+class color:
+    def __init__(self):
+        self.base_color = "#4153A3"
+        self.disable_buttons = "#535769"
+        self.fine = "#DF3939"
+        self.sidebar = "#292B34"
+        self.book_base = "#EEE8F2"
+        self.new_button_color = "#1A2032"
+        self.new_button_color_hover = "#090B12"
+
+colors = color()
 set_appearance_mode("light")
 from PIL import Image
 
 class Book_icon:
     def __init__(self,master:CTkFrame,logo:str,book_name:str,author:str,edition:int,copies_available,favourite:bool,reserved:bool):
-        self.frame = CTkFrame(master=master,width=360,height=280,fg_color=colors.book_base)
+        self.frame = CTkFrame(master=master,width=360,height=280,fg_color=colors.book_base,corner_radius=0)
         self.title = book_name
         self.author = author
         self.logo_img = CTkImage(Image.open(logo),size=(180,280))
@@ -39,31 +49,55 @@ class Book_icon:
         self.copies_label.place(x=200,y=140)
 
         self.reserve_btn = CTkButton(self.frame,
-                                     text="      Unreserve" if self.is_reserved else "      Reserve",
+                                     text="                Unreserve" if self.is_reserved else "                Reserve",
                                      font=("roboto",12,"bold"),
                                      width=180,
                                      height=40,
                                      corner_radius=0,
-                                     fg_color=colors.base_color,
-                                     image=CTkImage(Image.open("resources/icons/reserve.png"),size=(15,15)),compound="left",
-                                     hover_color=colors.disable_buttons,
-                                     anchor="w")
+                                     fg_color=colors.new_button_color,
+                                    #  image=CTkImage(Image.open("resources/icons/reserve.png"),size=(15,15)),compound="left",
+                                     hover_color=colors.new_button_color_hover,
+                                     anchor="w",
+                                     command=self.reserve_btn_click)
+        self.reserve_btn_img = CTkImage(Image.open("resources/icons/unreserve.png" if self.is_reserved else "resources/icons/reserve.png"),size=(17,17))
+        self.reserve_btn_img_label = CTkLabel(self.frame,image=self.reserve_btn_img,text="",fg_color=colors.new_button_color,bg_color=colors.new_button_color)
+        self.reserve_btn_img_label.place(x=200,y=185)
         # self.reserve_btn.grid(row=4,column=1,padx=0,pady=0)
         self.reserve_btn.place(x=180,y=180)
 
         self.favourite_btn = CTkButton(self.frame,
-                                       text="      Favourites",
+                                       text="                Favourites",
                                        font=("roboto",12,"bold"),
                                        width=180,
                                        height=40,
                                        corner_radius=0,
-                                       fg_color=colors.base_color,
-                                       image=CTkImage(Image.open("resources/icons/favourites.png"),size=(15,15)),compound="left",
-                                       hover_color=colors.disable_buttons,
-                                       anchor="w")
+                                       fg_color=colors.new_button_color,
+                                       hover_color=colors.new_button_color_hover,
+                                       anchor="w",
+                                       command=self.favourite_btn_click)
+        
         # self.favourite_btn.grid(row=5,column=1,padx=0,pady=0)
+        self.favourite_btn_img = CTkImage(Image.open("resources/icons/unfavourite2.png" if self.is_favourite else "resources/icons/favourites.png"),size=(17,17))
+        self.favourite_btn_img_label = CTkLabel(self.frame,image=self.favourite_btn_img,text="",fg_color=colors.new_button_color,bg_color=colors.new_button_color)
+        self.favourite_btn_img_label.place(x=200,y=225)
+
         self.favourite_btn.place(x=180,y=220)
     
+    def favourite_btn_click(self):
+        if self.is_favourite:
+            self.favourite_btn_img_label.configure(image = CTkImage(Image.open("resources/icons/favourites.png"),size=(17,17)))
+            self.is_favourite = False
+        else:
+            self.favourite_btn_img_label.configure(image = CTkImage(Image.open("resources/icons/unfavourite2.png"),size=(17,17)))
+            self.is_favourite = True
+
+    def reserve_btn_click(self):
+        if self.is_reserved:
+            self.reserve_btn_img_label.configure(image = CTkImage(Image.open("resources/icons/reserve.png"),size=(17,17)))
+            self.is_reserved = False
+        else:
+            self.reserve_btn_img_label.configure(image = CTkImage(Image.open("resources/icons/unreserve.png"),size=(17,17)))
+            self.is_reserved = True
     
     def pack(self,padx=0,pady=0):
         self.frame.pack(padx=padx,pady=pady)
