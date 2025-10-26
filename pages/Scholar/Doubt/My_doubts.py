e6f7ff9ca39064a2ec22792e0dc4c8ad90f1ad40
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2025-10-26 20:54:04 +0530

    scholar doubt section complete

--- pages/Scholar/Doubt/My_doubts.py
+++ pages/Scholar/Doubt/My_doubts.py
@@ -1,17 +1,22 @@
 from tkinter import *
 from customtkinter import *
 from PIL import Image
+from components import doubt_bubble
+from backend import doubts_logic
 
 class MyDoubtsPage:
     def __init__(self,master,scholar_record):
         self.scholar_record = scholar_record
         self.master = master
-        self.frame = CTkFrame(self.master,
+        self.frame = CTkScrollableFrame(self.master,
                               fg_color="#ffffff",
                               bg_color="#ffffff")
-        self.label = CTkLabel(self.frame,
-                              text="Scholar My doubts")
-        self.label.pack()    
+        self.show_doubts()
+    def show_doubts(self):
+        doubts = doubts_logic.get_my_doubts(self.scholar_record["scholar id"])
+        for doubt in doubts:
+            doubt_icon = doubt_bubble.Doubt(self.frame,doubt[0],doubt[1],doubt[2],self.scholar_record["scholar id"])
+            doubt_icon.pack()
     def pack(self,padx=0,pady=0):
         self.frame.pack(padx=padx,pady=pady,fill = "both",expand = True)
     def pack_forget(self):
